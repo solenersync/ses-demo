a382cf2398320f6fdf72d58f2af8a0a23e46c278
Brian Kinsella <brian.kinsella@distilled.ie>
2023-01-17 20:13:08 +0000

    add app

---
+++ src/main/java/com/solenersync/sesdemo/HelloController.java
@@ -0,0 +1,13 @@
+package com.solenersync.sesdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello from Solenersync";
+    }
+}
